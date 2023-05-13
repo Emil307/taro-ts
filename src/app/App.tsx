4908f7faea3ba897b4c93b160b636ea50f9643d7
unknown <emilnovikov392@gmail.com>
2023-05-13 16:56:03 +0600

    add pages add routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from '../pages/Landing';
+import AuthPage from '../pages/AuthPage';
+import SignUpPage from '../pages/SignUpPage';
 import './index.css';
 
 const App: React.FC = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing/>} />
+        <Route path='/auth' element={<AuthPage/>} />
+        <Route path='/signUp' element={<SignUpPage/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
