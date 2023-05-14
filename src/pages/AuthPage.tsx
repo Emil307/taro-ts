681f3272603a73a6215820664b82ffd3d0094fcf
unknown <emilnovikov392@gmail.com>
2023-05-14 15:22:22 +0600

    add styled pages authpage signup page

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import styled from 'styled-components';
 import AuthForm from '../features/AuthForm';
+import styled from 'styled-components';
+import bgImg from '../images/bg.jpg';
 
-const AuthPage: React.FC = () => {
+const Page = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${bgImg});
+`
 
+const Content = styled.div`
+  width: 400px;
+  height: 600px;
+  background: #fff;
+  border-radius: 20px;
+`
+
+const SignUpPage: React.FC = () => {
   return (
-    <><AuthForm/></>
+    <Page>
+      <Content>
+        <AuthForm/>
+      </Content>
+    </Page>
   )
 }
 
-export default AuthPage
+export default SignUpPage
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import SignUpForm from '../features/SignUpForm';
+import styled from 'styled-components';
+import bgImg from '../images/bg.jpg';
+
+const Page = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${bgImg});
+`
+
+const Content = styled.div`
+  width: 400px;
+  height: 600px;
+  background: #fff;
+  border-radius: 20px;
+`
 
 const SignUpPage: React.FC = () => {
   return (
-    <div><SignUpForm/></div>
+    <Page>
+      <Content>
+        <SignUpForm/>
+      </Content>
+    </Page>
   )
 }
 
